e17a1611c35f722947b381733668a863af34e7ab
Huracan <huracan22505@protonmail.com>
2020-12-09 01:04:01 +0200

    adds filter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './css/styles.css';
 import './js/dish-collection';
+import './js/filter';
 import './js/theme-switcher';
--- src/js/dish-collection.js
+++ src/js/dish-collection.js
@@ -1,48 +1,55 @@
 import foodCardTpl from '../templates/food-card.hbs';
+import filterTpl from '../templates/filter.hbs';
 import menu from '../data/menu.json';
 
 const menuContainer = document.querySelector('.js-menu');
-const filterButton = document.querySelector('.filter-button');
+const filter = document.querySelector('.filter');
 
-filterButton.addEventListener('click', onFilterButtonClick);
+filter.insertAdjacentHTML('beforeend', createFilterMarkup(menu));
 
-function onFilterButtonClick(e) {
-  menuContainer.innerHTML = '';
+filter.addEventListener('click', onFilterButtonClick);
 
-  createMenuCardMarkup(menu);
-  menuContainer.insertAdjacentHTML('beforeend', cardMarkup);
+createMarkup(menu);
+
+function createMarkup(el) {
+  menuContainer.insertAdjacentHTML('beforeend', createMenuCardMarkup(el));
 }
-
-let result = [];
-console.log('result', result);
-
-function onIngredientClick(e) {
-  if (!e.target.classList.contains('tag-list__item')) {
-    return;
-  }
-  let value = e.target.textContent;
-  menu.map(el => {
-    if (el.ingredients.includes(value)) {
-      result.push(el);
-    }
-  });
-
-  menuContainer.innerHTML = '';
-
-  createMenuCardMarkup(result);
-  menuContainer.insertAdjacentHTML('afterbegin', createMenuCardMarkup(result));
-
-  result = [];
-}
-
-const cardMarkup = createMenuCardMarkup(menu);
-menuContainer.insertAdjacentHTML('beforeend', cardMarkup);
 
 function createMenuCardMarkup(menu) {
   return menu.map(foodCardTpl).join('');
 }
 
-// const filterItem = document.querySelectorAll('.tag-list__item');
-// const card = document.querySelectorAll('.menu__item');
+function createFilterMarkup(menu) {
+  const array = menu
+    .reduce((acc, value) => [...acc, ...value.ingredients], [])
+    .filter((v, i, a) => a.indexOf(v) === i)
+    .sort();
 
-menuContainer.addEventListener('click', onIngredientClick);
+  return filterTpl(array);
+}
+
+function onFilterButtonClick(e) {
+  if (!e.target.classList.contains('filter-button')) {
+    return;
+  }
+
+  let result = [];
+  const value = e.target.textContent;
+
+  if (value === 'Все ингридиенты') {
+    menuContainer.innerHTML = '';
+    createMarkup(menu);
+  } else {
+    menu.map(el => {
+      if (el.ingredients.includes(value)) {
+        result.push(el);
+      }
+    });
+
+    menuContainer.innerHTML = '';
+
+    createMarkup(result);
+
+    result = [];
+  }
+}
